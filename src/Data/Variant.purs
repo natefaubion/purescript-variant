6669ce236197de109d4038cd0d816bc0fdbdd71f
Nathan Faubion <nathan@n-son.com>
2017-04-22 10:12:14 -0500

    Initial commit

---
+++ src/Data/Variant.purs
@@ -0,0 +1,98 @@
+module Data.Variant
+  ( Variant
+  , inj
+  , prj
+  , on
+  , case_
+  , default
+  , module Exports
+  ) where
+
+import Prelude
+import Data.Maybe (Maybe(..))
+import Data.Symbol (SProxy, class IsSymbol, reflectSymbol)
+import Data.Symbol (SProxy(..)) as Exports
+import Data.Tuple (Tuple(..))
+import Partial.Unsafe (unsafeCrashWith)
+import Unsafe.Coerce (unsafeCoerce)
+
+data Variant (a ∷ # Type)
+
+-- | Inject into the variant at a given label.
+-- | ```purescript
+-- | intAtFoo :: forall r. Variant (foo :: Int | r)
+-- | intAtFoo = inj (SProxy :: SProxy "foo") 42
+-- | ```
+inj
+  ∷ ∀ sym a r1 r2
+  . RowCons sym a r1 r2
+  ⇒ IsSymbol sym
+  ⇒ SProxy sym
+  → a
+  → Variant r2
+inj p a = coerceV (Tuple (reflectSymbol p) a)
+  where
+  coerceV ∷ Tuple String a → Variant r2
+  coerceV = unsafeCoerce
+
+-- | Attempt to read a variant at a given label.
+-- | ```purescript
+-- | case prj (SProxy :: SProxy "foo") intAtFoo of
+-- |   Just i  -> i + 1
+-- |   Nothing -> 0
+-- | ```
+prj
+  ∷ ∀ sym a r1 r2
+  . RowCons sym a r1 r2
+  ⇒ IsSymbol sym
+  ⇒ SProxy sym
+  → Variant r2
+  → Maybe a
+prj p = on p Just (const Nothing)
+
+-- | Attempt to read a variant at a given label by providing branches.
+-- | The failure branch receives the provided variant, but with the label
+-- | removed.
+on
+  ∷ ∀ sym a b r1 r2
+  . RowCons sym a r1 r2
+  ⇒ IsSymbol sym
+  ⇒ SProxy sym
+  → (a → b)
+  → (Variant r1 → b)
+  → Variant r2
+  → b
+on p f g r =
+  case coerceV r of
+    Tuple tag a | tag == reflectSymbol p → f a
+    _ → g (coerceR r)
+  where
+  coerceV ∷ Variant r2 → Tuple String a
+  coerceV = unsafeCoerce
+
+  coerceR ∷ Variant r2 → Variant r1
+  coerceR = unsafeCoerce
+
+-- | Combinator for exhaustive pattern matching.
+-- | ```purescript
+-- | caseFn :: Variant (foo :: Int, bar :: String, baz :: Boolean) -> String
+-- | caseFn = case_
+-- |  # on (SProxy :: SProxy "foo") (\foo -> "Foo: " <> show foo)
+-- |  # on (SProxy :: SProxy "bar") (\bar -> "Bar: " <> bar)
+-- |  # on (SProxy :: SProxy "baz") (\baz -> "Baz: " <> show baz)
+-- | ```
+case_ ∷ ∀ a. Variant () → a
+case_ r = unsafeCrashWith error
+  where
+  Tuple tag _ = unsafeCoerce r
+  error = "Data.Variant: pattern match failure [" <> tag <> "]"
+
+-- | Combinator for partial matching with a default value in case of failure.
+-- | ```purescript
+-- | caseFn :: Variant (foo :: Int, bar :: String, baz :: Boolean) -> String
+-- | caseFn = default "No match"
+-- |  # on (SProxy :: SProxy "foo") (\foo -> "Foo: " <> show foo)
+-- |  # on (SProxy :: SProxy "bar") (\bar -> "Bar: " <> bar)
+-- | ```
+default ∷ ∀ a r. a → Variant r → a
+default a _ = a
---
+++ test/Main.purs
@@ -0,0 +1,57 @@
+module Test.Main where
+
+import Prelude
+import Control.Monad.Eff (Eff)
+import Data.Maybe (Maybe(..))
+import Data.Variant (Variant, on, case_, default, inj, prj, SProxy(..))
+import Test.Assert (assert', ASSERT)
+
+type TestVariants =
+  ( foo ∷ Int
+  , bar ∷ String
+  , baz ∷ Boolean
+  )
+
+_foo ∷ SProxy "foo"
+_foo = SProxy
+
+_bar ∷ SProxy "bar"
+_bar = SProxy
+
+_baz ∷ SProxy "baz"
+_baz = SProxy
+
+foo ∷ ∀ r. Variant (foo ∷ Int | r)
+foo = inj _foo 42
+
+bar ∷ ∀ r. Variant (bar ∷ String | r)
+bar = inj _bar "bar"
+
+baz ∷ ∀ r. Variant (baz ∷ Boolean | r)
+baz = inj _baz true
+
+main :: Eff (assert ∷ ASSERT) Unit
+main = do
+  assert' "prj: Foo" $ prj _foo foo == Just 42
+  assert' "prj: !Foo" $ prj _foo bar == Nothing :: Maybe Int
+
+  let
+    case1 ∷ Variant TestVariants → String
+    case1 = case_
+      # on _foo (\a → "foo: " <> show a)
+      # on _bar (\a → "bar: " <> a)
+      # on _baz (\a → "baz: " <> show a)
+
+  assert' "case1: foo" $ case1 foo == "foo: 42"
+  assert' "case1: bar" $ case1 bar == "bar: bar"
+  assert' "case1: baz" $ case1 baz == "baz: true"
+
+  let
+    case2 ∷ Variant TestVariants → String
+    case2 = default "no match"
+      # on _foo (\a → "foo: " <> show a)
+      # on _bar (\a → "bar: " <> a)
+
+  assert' "case2: foo" $ case2 foo == "foo: 42"
+  assert' "case2: bar" $ case2 bar == "bar: bar"
+  assert' "case2: baz" $ case2 baz == "no match"
